b4e51c63c5f111f99c2c9da18ffe3a6d945214e8
Xie Yuheng <xyheme@gmail.com>
2025-04-15 20:51:13 +0800

    up

--- xbindkeys/xbindkeysrc.scm
+++ xbindkeys/xbindkeysrc.scm
@@ -1,7 +1,8 @@
 ;; ~/xbindkeysrc.scm
 
-(xbindkey '(shift mod1 x) "emacsclient -c -e \"(eshell t)\"")
-(xbindkey '(shift mod1 "c:36") "alacritty --working-directory $(xcwd)")
-(xbindkey '(control mod1 "c:36") "rofi -font \"Monaco 32\" -show run")
+(xbindkey '(shift mod1 x) "emacsclient -c -e '(eshell t)'")
+;; (xbindkey '(shift mod1 "c:36") "alacritty --working-directory $(xcwd)")
+(xbindkey '(shift mod1 "c:36") "gnome-terminal --working-directory $(xcwd)")
+(xbindkey '(control mod1 "c:36") "rofi -font 'unifont 36' -show run")
 (xbindkey '(control F9) "i3lock")
 (xbindkey '(control F10) "scrot")
