6049940042b3789483ca0fe779e536357ca4f5a0
Xie Yuheng <xyheme@gmail.com>
2025-04-15 22:24:14 +0800

    up

--- xbindkeys/xbindkeysrc.scm
+++ xbindkeys/xbindkeysrc.scm
@@ -1,8 +1,9 @@
 ;; ~/xbindkeysrc.scm
 
 (xbindkey '(shift mod1 x) "emacsclient -c -e '(eshell t)'")
-;; (xbindkey '(shift mod1 "c:36") "alacritty --working-directory $(xcwd)")
-(xbindkey '(shift mod1 "c:36") "gnome-terminal --working-directory $(xcwd) --hide-menubar")
+;; (xbindkey '(shift mod1 "c:36") "konsole --workdir $(xcwd)")
+;; (xbindkey '(shift mod1 "c:36") "gnome-terminal --working-directory $(xcwd) --hide-menubar")
+(xbindkey '(shift mod1 "c:36") "xfce4-terminal --working-directory $(xcwd) --hide-menubar")
 (xbindkey '(control mod1 "c:36") "rofi -font 'unifont 36' -show run")
 (xbindkey '(control F9) "i3lock")
 (xbindkey '(control F10) "scrot")
