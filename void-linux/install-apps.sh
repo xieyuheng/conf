f1d4695f9e8f963da38da20a5e0a84710abfdefa
Xie Yuheng <xyheme@gmail.com>
2025-04-24 16:49:54 +0800

    up

---
+++ void-linux/install-apps.sh
@@ -0,0 +1,21 @@
+sudo xbps-install \
+    rsync fish-shell starship emacs-gtk3 ranger zfs \
+    git tig nodejs \
+    mpv vlc ffmpeg i3 i3status tree wget curl \
+    alacritty xfce4-terminal \
+    clang valgrind cloc gdb \
+    libX11-devel SDL2-devel \
+    proxychains-ng vim zsh ncdu \
+    noto-fonts-cjk noto-fonts-ttf noto-fonts-ttf-extra \
+    fastfetch htop btop \
+    rofi feh scrot xclip xbindkeys \
+    light flameshot network-manager-applet cbatticon \
+    ugrep the_silver_searcher \
+    okular mupdf zathura zathura-djvu zathura-pdf-mupdf zathura-ps \
+    djview calibre \
+    goldendict \
+    amber onefetch \
+    xorg xorg-fonts \
+    sof-firmware alsa-firmware alsa-utils \
+    chromium firefox \
+    gimp blender
