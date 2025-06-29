c2ec01904fbcae0a6574a44f54ddd4d7d1798b3d
Xie Yuheng <xyheme@gmail.com>
2025-06-29 18:39:33 +0800

    up

--- xbindkeys/xbindkeysrc.scm
+++ xbindkeys/xbindkeysrc.scm
@@ -5,6 +5,6 @@
 ;; (xbindkey '(shift mod1 "c:36") "gnome-terminal --working-directory $(xcwd) --hide-menubar")
 (xbindkey '(shift mod1 "c:36") "xfce4-terminal --working-directory $(xcwd) --hide-menubar")
 ;; (xbindkey '(control mod1 "c:36") "rofi -font 'unifont 36' -show run")
-(xbindkey '(control mod1 "c:36") "dmenu_run -fn unifont-16")
+(xbindkey '(control mod1 "c:36") "dmenu_run -fn unifont-16 -l 20")
 (xbindkey '(control F9) "i3lock")
 (xbindkey '(control F10) "scrot")
